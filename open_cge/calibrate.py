74dc3024efce4599183491201086a10a3fdb9a65
vpcfadri1 <vcfadri@up.edu.ph>
2025-03-09 13:36:19 +0800

    edited calibrate.py according to Nobuhiro et al.

--- open_cge/calibrate.py
+++ open_cge/calibrate.py
@@ -18,69 +18,69 @@ class model_data(object):
     """
 
     def __init__(self, sam, h, ind):
-        # foreign saving
+        # foreign saving Y
         self.Sf0 = DataFrame(sam, index=["INV"], columns=["EXT"])
-        # private saving
+        # private saving Y
         self.Sp0 = DataFrame(sam, index=["INV"], columns=["HOH"])
-        # government saving/budget balance
+        # government saving/budget balance Y
         self.Sg0 = DataFrame(sam, index=["INV"], columns=["GOV"])
-        # repatriation of profits
-        self.Fsh0 = DataFrame(sam, index=["EXT"], columns=["HOH"])
-        # capital stock
-        self.Kk0 = 10510
-        # foreign-owned capital stock
-        self.Kf0 = 6414.35
-        # domestically-owned capital stock
-        self.Kd0 = self.Kk0 - self.Kf0
+        # repatriation of profits N
+        # self.Fsh0 = DataFrame(sam, index=["EXT"], columns=["HOH"])
+        # capital stock N
+        # self.Kk0 = 10510
+        # foreign-owned capital stock N
+        # self.Kf0 = 6414.35
+        # domestically-owned capital stock N
+        # self.Kd0 = self.Kk0 - self.Kf0
 
-        # direct tax
-        self.Td0 = DataFrame(sam, index=["DTX"], columns=["HOH"])
-        # transfers
-        self.Trf0 = DataFrame(sam, index=["HOH"], columns=["GOV"])
+        # direct tax Y
+        self.Td0 = DataFrame(sam, index=["GOV"], columns=["HOH"])
+        # transfers (not found) N 
+        # self.Trf0 = DataFrame(sam, index=["HOH"], columns=["GOV"]) 
         # production tax
-        self.Tz0 = DataFrame(sam, index=["ACT"], columns=list(ind))
-        # import tariff
-        self.Tm0 = DataFrame(sam, index=["IDT"], columns=list(ind))
+        self.Tz0 = DataFrame(sam, index=["IDT"], columns=list(ind))
+        # import tariff correct
+        self.Tm0 = DataFrame(sam, index=["TRF"], columns=list(ind))
 
-        # the h-th factor input by the j-th firm
+        # the h-th factor input by the j-th firm correct Y
         self.F0 = DataFrame(sam, index=list(h), columns=list(ind))
-        # factor endowment of the h-th factor
+        # factor endowment of the h-th factor correct Y
         self.Ff0 = self.F0.sum(axis=1)
-        # composite factor (value added)
-        self.Y0 = self.F0.sum(axis=0)
-        # intermediate input
-        self.X0 = DataFrame(sam, index=list(ind), columns=list(ind))
-        # total intermediate input by the j-th sector
-        self.Xx0 = self.X0.sum(axis=0)
-        # output of the i-th good
-        self.Z0 = self.Y0 + self.Xx0
+        # composite factor (value added) Y
+        self.Y0 = self.F0.sum(axis=0) 
+        # intermediate input Y
+        self.X0 = DataFrame(sam, index=list(ind), columns=list(ind)) 
+        # total intermediate input by the j-th sector Y
+        self.Xx0 = self.X0.sum(axis=0)  #intermediate value, sum of X0
+        # output of the i-th good Y
+        self.Z0 = self.Y0 + self.Xx0 
 
-        # household consumption of the i-th good
+        # household consumption of the i-th good Y
         self.Xp0 = DataFrame(sam, index=list(ind), columns=["HOH"])
-        # government consumption
+        # government consumption Y
         self.Xg0 = DataFrame(sam, index=list(ind), columns=["GOV"])
-        # investment demand
+        # investment demand Y
         self.Xv0 = DataFrame(sam, index=list(ind), columns=["INV"])
-        # exports
+        # exports Y
         self.E0 = DataFrame(sam, index=list(ind), columns=["EXT"])
         self.E0 = self.E0["EXT"]
-        # imports
+        # imports Y
         self.M0 = DataFrame(sam, index=["EXT"], columns=list(ind))
-        self.M0 = self.M0.loc["EXT"]
+        self.M0 = self.M0.loc["EXT"]  
 
-        # domestic supply/Armington composite good
+        # domestic supply/Armington composite good Y
         self.Q0 = (
             self.Xp0["HOH"]
             + self.Xg0["GOV"]
             + self.Xv0["INV"]
             + self.X0.sum(axis=1)
         )
-        # production tax rate
+        # production tax rate Y
         tauz = self.Tz0 / self.Z0
-        # domestic tax rate
+        # domestic tax rate Y
         self.D0 = (1 + tauz.loc["ACT"]) * self.Z0 - self.E0
 
-        # Compute aggregates
+        # Compute aggregates N; Intermediate Values
 
         # aggregate output
         self.Yy0 = self.Y0.sum()
@@ -101,10 +101,10 @@ class model_data(object):
         # interest rate
         self.R0 = self.Ff0["CAP"] / self.Kk0
 
-        # export price index
+        # export price index Y
         self.pWe = np.ones(len(ind))
         self.pWe = Series(self.pWe, index=list(ind))
-        # import price index
+        # import price index Y
         self.pWm = np.ones(len(ind))
         self.pWm = Series(self.pWm, index=list(ind))
 
@@ -124,46 +124,48 @@ class parameters(object):
 
     def __init__(self, d, ind):
 
-        # elasticity of substitution
-        self.sigma = [3, 1.2, 3, 3]
-        self.sigma = Series(self.sigma, index=list(ind))
-        # substitution elasticity parameter
+        # elasticity of substitution; Y constant in GAMS sample
+        self.sigma = Series(2, index=list(ind))
+        # substitution elasticity parameter Y
         self.eta = (self.sigma - 1) / self.sigma
 
-        # elasticity of transformation
-        self.psi = [3, 1.2, 3, 3]
-        self.psi = Series(self.psi, index=list(ind))
-        # transformation elasticity parameter
+        # elasticity of transformation; Y constant in GAMS sample
+        self.psi = Series(2, index=list(ind))
+        # transformation elasticity parameter Y
         self.phi = (self.psi + 1) / self.psi
 
-        # share parameter in utility function
+        # share parameter in utility function Y
         self.alpha = d.Xp0 / d.XXp0
-        self.alpha = self.alpha["HOH"]
-        # share parameter in production function
+        self.alpha = self.alpha["HOH"]      #ONLY HOH?
+        # share parameter in production function Y
         self.beta = d.F0 / d.Y0
         temp = d.F0**self.beta
-        # scale parameter in production function
+        # scale parameter in production function Y
         self.b = d.Y0 / temp.prod(axis=0)
 
-        # intermediate input requirement coefficient
+        # intermediate input requirement coefficient Y
         self.ax = d.X0 / d.Z0
-        # composite factor input requirement coefficient
+        # composite factor input requirement coefficient Y
         self.ay = d.Y0 / d.Z0
         self.mu = d.Xg0 / d.XXg0
-        # government consumption share
-        self.mu = self.mu["GOV"]
-        self.lam = d.Xv0 / d.XXv0
+        # government consumption share  N, not over total investments, but savings
+        self.mu = self.mu["GOV"]  #only GOV?
+        Sp0 = self.Sp0.values[0, 0] 
+        Sg0 = self.Sg0.values[0, 0]
+        Sf0 = self.Sf0.values[0, 0]
+        total_savings = Sp0 +  Sg0 + Sf0
+        self.lam = d.Xv0 / total_savings
         # investment demand share
-        self.lam = self.lam["INV"]
+        self.lam = self.lam["INV"] #only INV?
 
-        # production tax rate
+        # production tax rate Y
         self.tauz = d.Tz0 / d.Z0
-        self.tauz = self.tauz.loc["ACT"]
-        # import tariff rate
+        self.tauz = self.tauz.loc["ACT"] #only ACT?
+        # import tariff rate Y
         self.taum = d.Tm0 / d.M0
-        self.taum = self.taum.loc["IDT"]
+        self.taum = self.taum.loc["IDT"] #only IDT?
 
-        # share parameter in Armington function
+        # share parameter in Armington function Y
         self.deltam = (
             (1 + self.taum)
             * d.M0 ** (1 - self.eta)
@@ -175,13 +177,13 @@ class parameters(object):
         self.deltad = d.D0 ** (1 - self.eta) / (
             (1 + self.taum) * d.M0 ** (1 - self.eta) + d.D0 ** (1 - self.eta)
         )
-
-        # scale parameter in Armington function
+            
+        # scale parameter in Armington function Y
         self.gamma = d.Q0 / (
             self.deltam * d.M0**self.eta + self.deltad * d.D0**self.eta
         ) ** (1 / self.eta)
 
-        # share parameter in transformation function
+        # share parameter in transformation function Y
         self.xie = d.E0 ** (1 - self.phi) / (
             d.E0 ** (1 - self.phi) + d.D0 ** (1 - self.phi)
         )
@@ -189,20 +191,21 @@ class parameters(object):
             d.E0 ** (1 - self.phi) + d.D0 ** (1 - self.phi)
         )
 
-        # scale parameter in transformation function
+        # scale parameter in transformation function Y
         self.theta = d.Z0 / (
             self.xie * d.E0**self.phi + self.xid * d.D0**self.phi
         ) ** (1 / self.phi)
 
-        # average propensity to save
+        # average propensity to save N, no transfers or repatriation values
         self.ssp = (
-            d.Sp0.values / (d.Ff0.sum() - d.Fsh0.values + d.Trf0.values)
+            d.Sp0.values / (d.Ff0.sum())
         )[0]
-        # direct tax rate
+        # direct tax rate Y
         self.taud = (d.Td0.values / d.Ff0.sum())[0]
-        # transfer rate
-        self.tautr = (d.Trf0.values / d.Ff0["LAB"])[0]
-        # government revenue
+        # transfer rate (Not Used)
+        # self.tautr = (d.Trf0.values / d.Ff0["LAB"])[0]
+        # government revenue, intermediate value
         self.ginc = d.Td0 + d.Tz0.sum() + d.Tm0.sum()
-        # household income
-        self.hinc = d.Ff0.sum()
+        self.ssg = d.Sg0 / self.ginc
+        # household income N
+        # self.hinc = d.Ff0.sum()
